a095b904cbd6b50e2ca4b49bd1a0995bc452f846
ARB-jasper <jasper.dofitas@arbcalls.com>
2020-03-05 00:05:07 +0800

    working laravel 6.2 using docker

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
-const mix = require('laravel-mix');
+const { EnvironmentPlugin, ProvidePlugin } = require("webpack");
+const mix = require("laravel-mix");
+const path = require("path");
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +12,62 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    plugins: [
+        new EnvironmentPlugin({
+            BASE_URL: "/"
+        }),
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+        new ProvidePlugin({
+            $: "jquery",
+            "window.jQuery": "jquery",
+            jquery: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /node_modules(?:\/|\\).+\.js$/,
+                loader: "babel-loader",
+                include: [path.join(__dirname, "node_modules/bootstrap-vue")]
+            }
+        ]
+    },
+
+    resolve: {
+        alias: {
+            "~": path.join(__dirname, "resources/js"),
+            node_modules: path.join(__dirname, "resources/js")
+        }
+    }
+});
+
+mix.scripts(["resources/assets/js/jquery.min.js"], "public/js/scripts.js");
+
+mix.js("resources/js/app.js", "public/js").sass(
+    "resources/sass/app.scss",
+    "public/css"
+);
+
+mix.styles(
+    [
+        "node_modules/bootstrap/dist/css/bootstrap.css",
+        "node_modules/@fortawesome/font-awesome-free/css/font-awesome.css"
+    ],
+    "public/css/shared.css"
+);
+
+mix.copy("resources/assets/images/", "public/images/");
+mix.copy("node_modules/@fortawesome/font-awesome-free/scss", "public/scss");
+
+mix.scripts(
+    [
+        "node_modules/jquery/dist/jquery.min.js",
+        "node_modules/bootstrap/dist/js/bootstrap.min.js"
+    ],
+    "public/js/shared.js"
+);
+
+mix.version();
