274ba7d181af7c02a3ca784629eefaa709d18ad3
ARB-jasper <jasper.dofitas@arbcalls.com>
2020-03-10 05:26:28 +0800

    added space on +

--- resources/js/components/templates/Header.vue
+++ resources/js/components/templates/Header.vue
@@ -2,17 +2,17 @@
     <div class="row no-gutters">
         <div class="col-12 text-center client-portal-header desktop-hide position-relative">
             <div class="client-portal-background-image">
-                <img class="w-100" :src="'images/'+portfolio.header_image_mobile_url" />
+                <img class="w-100" :src="'images/' + portfolio.header_image_mobile_url" />
             </div>
-            <img class="client-portal-header-logo" :src="'images/logo/'+portfolio.logo_url" />
+            <img class="client-portal-header-logo" :src="'images/logo/' + portfolio.logo_url" />
         </div>
         <div class="col-12 text-center client-portal-header mobile-hide position-relative">
             <div class="client-portal-background-image">
-                <img class="w-100" :src="'images/'+portfolio.header_image_url" />
+                <img class="w-100" :src="'images/' + portfolio.header_image_url" />
             </div>
             <img
                 class="client-portal-header-logo"
-                :src="'images/logo/'+portfolio.logo_url"
+                :src="'images/logo/' + portfolio.logo_url"
             />
         </div>
     </div>
@@ -23,6 +23,7 @@
 
 export default {
     Name: "Header",
+
     data() {
         return {
             portfolio: null
