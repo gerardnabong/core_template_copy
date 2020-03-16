3a90a9c6329d3dee90d012dc1be4c484b73998b5
ARB-jasper <59568363+ARB-jasper@users.noreply.github.com>
2020-03-17 03:02:41 +0800

    fix Issues

--- resources/js/components/pages/OnProcess.vue
+++ resources/js/components/pages/OnProcess.vue
@@ -5,16 +5,14 @@
                 <span
                     class="icon-computer-gear-icon client-portal-icon"
                     :style="{color: portfolio.button_color}"
-                ></span>
+                />
             </div>
         </div>
         <div class="row justify-content-center">
             <div class="col-md-6 text-center pt-4">
                 <h2 class="client-portal-heading-text">Your application is still on process</h2>
                 <div class="mt-5">
-                    <p
-                        class="client-portal-paragraph"
-                    >A customer representative will contact you soon!</p>
+                    <p class="client-portal-paragraph">A customer representative will contact you soon!</p>
                     <b-button
                         class="client-portal-button client-portal-btn-primary client-portal-btn-submit mt-3"
                         :style="{ 'background-color': clientPortalButton }"
@@ -40,17 +38,11 @@ import CallUsButton from '~/components/templates/buttons/CallUsButton';
 export default {
     name: 'OnProcess',
 
-    data() {
-        return {
-            portfolio: null,
-        };
-    },
-
     components: {
         CallUsButton,
     },
 
-    created() {
+    created () {
         this.portfolio = this.$jsVars.portfolio;
         this.clientPortalButton = this.portfolio.button_color;
     },
