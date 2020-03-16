5e983555da33c1c6bbc54917700d1ffcf78e7574
ARB-jasper <59568363+ARB-jasper@users.noreply.github.com>
2020-03-17 02:52:04 +0800

    Feature/error success (#8)
    * added Vuex Sample Code
    
    * initial commit for loginpage
    
    * created Login Page with missing hover effects
    
    * down applying custom button
    
    * change Icons from SVG to Fonts
    
    * fix PR of khen
    
    * removed unneeded files
    
    * added
    
    * Remove Custom Button
    
    * need to finalize responsive
    
    * added responsive
    
    * fix formatter issue
    
    * Merge branch 'develop' of https://github.com/ARBCalls/client_portal into feature-newLoanPage
    
    * fix formating issue
    
    * created loan transfer
    
    * fix design
    
    * fixed design
    
    * done with new-loan
    
    * Merge branch 'feature-newLoanPage' of https://github.com/ARBCalls/client_portal into feature/loanTransfer
    
    * fix issue in button
    
    * Fix PR Albert
    
    * tapos na raw - khen
    
    * change double quotes to single quotes
    
    * fix PR
    
    * use new formater
    
    * Fix PR issue
    
    * Done with Design
    
    * revemo border on login button
    
    * fix PR
    
    * updated
    
    * Merge branch 'feature-newLoanPage' of https://github.com/ARBCalls/client_portal into feature/loanTransfer
    
    * for PR
    
    * for PR
    
    * added gitIgnore
    
    * Merge branch 'develop' of https://github.com/ARBCalls/client_portal into feature/errorSuccess
    
    * added space in index
    
    * fix PR Albert
    
    * fix PR issue
    
    * added space
    
    * edit ErrorPage

---
+++ resources/js/components/pages/ErrorPage.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center pt-4">
+                <span
+                    class="icon-sad-icon client-portal-icon"
+                    :style="{color: portfolio.button_color}"
+                />
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center pt-4">
+                <h2 class="client-portal-heading-text">Oops! Something went wrong</h2>
+                <div class="mt-5">
+                    <p class="client-portal-paragraph">
+                        The request failed for some reason, but you are still on track.
+                    </p>
+                    <b-button
+                        class="client-portal-button client-portal-btn-primary client-portal-btn-submit mt-3"
+                        :style="{ 'background-color': clientPortalButton }"
+                        @mouseover="clientPortalButton = portfolio.button_hover_color"
+                        @mouseleave="clientPortalButton = portfolio.button_color"
+                    >Try Again</b-button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col d-flex justify-content-center my-5">
+                <call-us-button />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+'use strict';
+
+import CallUsButton from '~/components/templates/buttons/CallUsButton';
+
+export default {
+    name: 'ErrorPage',
+
+    components: {
+        CallUsButton,
+    },
+
+    created () {
+        this.portfolio = this.$jsVars.portfolio;
+        this.clientPortalButton = this.portfolio.button_color;
+    },
+};
+</script>
