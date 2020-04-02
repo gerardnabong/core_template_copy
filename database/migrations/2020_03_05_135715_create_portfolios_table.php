385aa39fc32251684898d57c2c7978c6802510c5
ARB-jasper <59568363+ARB-jasper@users.noreply.github.com>
2020-04-03 04:46:16 +0800

    remove Arr

--- database/migrations/2020_03_05_135715_create_portfolios_table.php
+++ database/migrations/2020_03_05_135715_create_portfolios_table.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Schema;
 use App\Model\Portfolio;
 
