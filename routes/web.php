54b8a5b250fd558ad6be921daa8163a4f265a6f9
ARB-jasper <59568363+ARB-jasper@users.noreply.github.com>
2020-04-09 21:41:15 +0800

    added health check link (#18)
    * added health check link
    
    * added Note Regarding Health check
    
    * fixed format
    
    * added Warrence name

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+/*
+    Note for in the codebuild process im injecting the app version so
+    that we can make sure if we are running the up to date version
+    so creating this health_check so that you don have to worry
+    about catching your domain not found error for now - Warrence Lim
+*/
+Route::get('/health_check', function () {
+    return env('APP_VERSION');
+});
+
 Route::get('/getPortfolio', 'WebsiteController@getPortfolio');
 
 // TODO this should be deleted later because we'd better use whitelist later on - Albert
