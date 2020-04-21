2a2d57f2360b9cde228f5cb0ac029042e3b12f85
ARB-jasper <59568363+ARB-jasper@users.noreply.github.com>
2020-04-22 02:30:28 +0800

    fix persistent login

--- resources/js/storage/store.js
+++ resources/js/storage/store.js
@@ -3,11 +3,14 @@
 import Vuex from "vuex";
 import Vue from "vue";
 import createPersistedState from "vuex-persistedstate";
+import Cookies from 'js-cookie';
 
 import Client from "./modules/Client";
 import ProgressBar from "./modules/ProgressBar";
 
 Vue.use(Vuex);
+
+const EXPIRE_IN_2HRS = 2/24;
 
 export default new Vuex.Store({
     modules: {
@@ -15,6 +18,10 @@ export default new Vuex.Store({
         Client,
     },
     plugins: [createPersistedState({
-        storage: window.sessionStorage,
+        storage: {
+            getItem: key => Cookies.get(key),
+            setItem: (key, value) => Cookies.set(key, value, { expires: EXPIRE_IN_2HRS }),
+            removeItem: key => Cookies.remove(key),
+        },
     })],
 });
