0c003b23d65fb78ca2f3a6178b45736cda3fd9fe
ARB-jasper <59568363+ARB-jasper@users.noreply.github.com>
2020-04-24 04:13:46 +0800

    hotfix multiple source api (#44)
    * hotfix multiple source api
    
    * change demo to dev
    
    * revert to demo
    
    * fix pr Joel

--- app/Model/Portfolio.php
+++ app/Model/Portfolio.php
@@ -16,20 +16,26 @@ class Portfolio extends Model
         [
             'display_name' => 'Inbox Credit',
             'lead_portfolio_id' => '19004',
+            'portfolio_api_url' => 'inbox-credit',
         ], [
             'display_name' => 'Better Day Loan',
             'lead_portfolio_id' => '19007',
+            'portfolio_api_url' => 'better-day',
         ], [
             'display_name' => 'First Loan',
             'lead_portfolio_id' => '19006',
+            'portfolio_api_url' => 'first-loan',
         ], [
             'display_name' => 'Comet Loans',
             'lead_portfolio_id' => '19003',
+            'portfolio_api_url' => 'comet',
         ], [
             'display_name' => 'Inbox Loan',
             'lead_portfolio_id' => '19000',
+            'portfolio_api_url' => 'inbox-loan',
         ]
     ];
+    const VERTICAL_URL = '.vertical.arbcalls.com/';
 
     public static function getPortfolio(): Portfolio
     {
@@ -46,5 +52,14 @@ class Portfolio extends Model
     public function client(): HasMany
     {
         return $this->hasMany(Client::class);
+    }
+
+    public function getPortfolioApiUrl(string $url): string
+    {
+        $suffix = '';
+        if (config_safe('app.env')  !== 'production') {
+            $suffix = '-demo';
+        }
+        return 'https://' . $this->portfolio_api_url . $suffix . self::VERTICAL_URL . $url;
     }
 }
