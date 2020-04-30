7c1fb05b101224be74eadb5694fa1469823d3a09
ARB-jasper <59568363+ARB-jasper@users.noreply.github.com>
2020-05-01 04:23:08 +0800

    fixing router-push

--- resources/js/router/router.js
+++ resources/js/router/router.js
@@ -165,7 +165,6 @@ const ROUTES = [
         path: "/success",
         component: SuccessPage,
         meta: {
-            requiredClientStatus: constants.CLIENT_STATUS_LOAN_ON_GOING_CLIENT_ID,
             title: 'Success',
             metaTags: [
                 {
